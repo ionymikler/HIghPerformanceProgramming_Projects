e640ddc718a5e31bbeb1e058f7e1df5f6a09020b
Jonathan Mikler <s222962@dtu.dk>
2024-01-04 11:07:53 +0100

    all iterations of part 2 implemented and tested

--- project_1/matrix_lib/matmul_permutations.c
+++ project_1/matrix_lib/matmul_permutations.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "matmul_permutations.h"
 
+void init_C(int m,int n,double **C){
+    int i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            C[i][j] = 0.0;
+        }
+    }
+}
+
 void multiplicationMatrix(int N, int M, int K, int **matrixA, int **matrixB, int **resultMatrix)
 {
     for (int i = 0; i < M; i++)
@@ -17,13 +28,11 @@ void multiplicationMatrix(int N, int M, int K, int **matrixA, int **matrixB, int
 }
 
 void matmult_mnk(int m,int n,int k,double **A,double **B,double **C){
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            C[i][j] = 0;
-            for (int q = 0; q < k; q++)
-            {
+    init_C(m,n,C);
+
+    for (int i = 0; i < m; i++){
+        for (int j = 0; j < n; j++){
+            for (int q = 0; q < k; q++){
                 C[i][j] += A[i][q] * B[q][j];
             }
         }
@@ -31,12 +40,61 @@ void matmult_mnk(int m,int n,int k,double **A,double **B,double **C){
 } 
 
 void matmult_nmk(int m,int n,int k,double **A,double **B,double **C){
+    init_C(m,n,C);
+
     for (int col=0; col < n; col++){
         for (int row=0; row <m; row++){
-            C[row][col]=0;
             for (int q=0; q<k; q++){
                 C[row][col] += A[row][q] * B[q][col];
             }
         }
     }
-} 
+}
+
+void matmult_mkn(int m,int n,int k,double **A,double **B,double **C){
+    init_C(m,n,C);
+
+    for (int row=0; row <m; row++){
+        for (int q=0; q<k; q++){
+            for (int col=0; col < n; col++){
+                C[row][col] += A[row][q] * B[q][col];
+            }
+        }
+    }
+}
+
+void matmult_nkm(int m,int n,int k,double **A,double **B,double **C){
+    init_C(m,n,C);
+
+    for (int col=0; col < n; col++){
+        for (int q=0; q<k; q++){
+            for (int row=0; row <m; row++){
+                C[row][col] += A[row][q] * B[q][col];
+            }
+        }
+    }
+}
+
+void matmult_kmn(int m,int n,int k,double **A,double **B,double **C){
+    init_C(m,n,C);
+
+    for (int q=0; q<k; q++){
+        for (int row=0; row <m; row++){
+            for (int col=0; col < n; col++){
+                C[row][col] += A[row][q] * B[q][col];
+            }
+        }
+    }
+}
+
+void matmult_knm(int m,int n,int k,double **A,double **B,double **C){
+    init_C(m,n,C);
+
+    for (int q=0; q<k; q++){
+        for (int col=0; col < n; col++){
+            for (int row=0; row <m; row++){
+                C[row][col] += A[row][q] * B[q][col];
+            }
+        }
+    }
+}
