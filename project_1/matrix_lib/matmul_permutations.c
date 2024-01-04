5c25a7e2cc88f7fb2a15a67aa3abe2550a547040
Jonathan Mikler <s222962@dtu.dk>
2024-01-04 10:25:33 +0100

    qip

--- project_1/matrix_lib/matmul_permutations.c
+++ project_1/matrix_lib/matmul_permutations.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "part2.h"
+#include "matmul_permutations.h"
 
 void multiplicationMatrix(int N, int M, int K, int **matrixA, int **matrixB, int **resultMatrix)
 {
@@ -22,10 +22,21 @@ void matmult_mnk(int m,int n,int k,double **A,double **B,double **C){
         for (int j = 0; j < n; j++)
         {
             C[i][j] = 0;
-            for (int k = 0; k < k; k++)
+            for (int q = 0; q < k; q++)
             {
-                C[i][j] += A[i][k] * B[k][j];
+                C[i][j] += A[i][q] * B[q][j];
             }
         }
     }
-}
+} 
+
+void matmult_nmk(int m,int n,int k,double **A,double **B,double **C){
+    for (int col=0; col < n; col++){
+        for (int row=0; row <m; row++){
+            C[row][col]=0;
+            for (int q=0; q<k; q++){
+                C[row][col] += A[row][q] * B[q][col];
+            }
+        }
+    }
+} 
