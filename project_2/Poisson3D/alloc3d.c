d4ceb5f9d87924e6de74261bd6d821d1a04102e6
Jonathan Mikler <s222962@dtu.dk>
2024-01-10 11:33:27 +0100

    populate range implemented

--- project_2/Poisson3D/alloc3d.c
+++ project_2/Poisson3D/alloc3d.c
@@ -36,3 +36,26 @@ free_3d(double ***p) {
     free(p[0][0]);
     free(p);
 }
+
+void populate_range(double ***p, int startsEnds[6], double val){
+    int x_s = startsEnds[0];
+    int x_e = startsEnds[1];
+
+    int y_s = startsEnds[2];
+    int y_e = startsEnds[3];
+    
+    int z_s = startsEnds[4];
+    int z_e = startsEnds[5];
+
+    for (int i=x_s;i<x_e;i++){
+        for (int j=y_s; j<y_e; j++){
+            for (int k=z_s; k<z_e; k++){
+                p[i][j][k] = val;
+            }
+        }
+    }   
+}
+
+// void populate_range(double ***p, double val){
+//     p[1][1][1] = val;
+// }
