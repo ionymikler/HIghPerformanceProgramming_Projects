14821c98faa554aa673023766c8a9dc113bc2e58
Jonathan Mikler <s222962@dtu.dk>
2024-01-10 11:33:27 +0100

    populate range implemented

--- project_2/Poisson3D/alloc3d.h
+++ project_2/Poisson3D/alloc3d.h
@@ -2,6 +2,8 @@
 #define __ALLOC_3D
 
 double ***malloc_3d(int m, int n, int k);
+void populate_range(double ***p, int startsEnds[6], double val);
+// void populate_range(double ***p, double val);
 
 #define HAS_FREE_3D
 void free_3d(double ***array3D);
