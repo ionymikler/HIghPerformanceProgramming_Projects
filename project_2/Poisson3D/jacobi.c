39408b6e9843a08607cd13faff653a846da21747
mihaimnd <mihaivandrei@gmail.com>
2024-01-10 15:11:39 +0100

    corrected Jacobi

--- project_2/Poisson3D/jacobi.c
+++ project_2/Poisson3D/jacobi.c
@@ -3,20 +3,15 @@
  */
 #include <math.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 double***
 jacobi(double*** input, double*** output, double*** f, int N, int iter_max, double tolerance) {
     //Delta 2 / N where N is the number of points 
     double delta = 2.0 / (double)N;
-    double dif = 10000;
+    double dif = 10000.0;
     double min_dif = .01;
     int max_step = 100;
-
-    // Calculate extend of the heater location 
-    int max_x_location = floor((double)N *-3/8);
-    int max_y_location = floor((double)N *-1/2);
-    int min_z_location = floor((double)N *-2/3);
-    int max_z_location = 0;
     
     // Create the first 3D matrix, all values are initialized to zero
     for (int i = 0; i < N;i++){
@@ -44,14 +39,23 @@ jacobi(double*** input, double*** output, double*** f, int N, int iter_max, doub
         }
     }
     // boundary (wall)  conditions for input as well 
-    input = output;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            for (int k = 0; k < N; k++) {
+                input[i][j][k] = output[i][j][k];
+            }
+        }
+    }
 
     // Loop through Steps
-    for (int steps = 0; steps < iter_max; steps++){ 
+    int steps = 0;
+    while ( steps < iter_max && dif > tolerance){
+        dif = 0;
          for (int i = 1; i < (N-1);i++){
             for (int j = 1; j < (N-1); j++){
                 for (int k = 1; k < (N-1); k++){
                 // Calculate new value - rewritten for readability.
+                    
                     output[i][j][k] = (input[i-1][j][k] + 
                                     input[i+1][j][k] +
                                     input[i][j-1][k] +
@@ -59,17 +63,26 @@ jacobi(double*** input, double*** output, double*** f, int N, int iter_max, doub
                                     input[i][j][k-1] +
                                     input[i][j][k+1] +
                                     pow(delta,2) * f[i][j][k])/ 6.0;
-
+                                                    
                     // Add norm
+                    dif += pow(output[i][j][k] - input[i][j][k], 2);
                 }
             }
         }
         // Calculate the difference criteria
-        
-        double dif = 100.0;//abs(input[0][0][0] - output[0][0][0]);
+        dif = sqrt(dif);
+        printf("dif %lf\n", dif);
+        //abs(input[0][0][0] - output[0][0][0]);
         // Make new input
-        input = output;
 
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    input[i][j][k] = output[i][j][k];
+                }
+            }
+        }
+        steps++;
     }
     return input;
 }
