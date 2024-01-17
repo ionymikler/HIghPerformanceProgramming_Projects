4abb861789597630f59a4d30fc24479fe71ae76b
Jonathan Mikler <s222962@dtu.dk>
2024-01-17 12:32:32 +0100

    Add offloading implementation for matrix multiplication

--- project_3/matmul/matmult_omp_lib.cpp
+++ project_3/matmul/matmult_omp_lib.cpp
@@ -1,7 +1,24 @@
 extern "C"{
 #include <stdio.h>
+#include <stdlib.h>
 #include <cblas.h>
+#include <omp.h>
 #include "matmult_omp_lib.h"
+
+void init_C_dev(int m, int n, double **C, int num_teams, int threads_per_team){
+    // Remember; array mapping is [lower:length], NOT [lower:upper]
+    #pragma omp target teams distribute parallel for \
+        map(to:m,n), map(from:C[0:m][0:n]) \
+        num_teams(num_teams) thread_limit(threads_per_team) // QUESTION: Should we hard code these values?
+        // collapse(2)
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++){
+                C[i][j] = 0.0;
+        }
+    }
+
+}
 
 void init_C(int m,int n,double **C){
     #pragma omp parallel for collapse(2) \
@@ -42,6 +59,22 @@ void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C){
         }
 }
 
+void matmult_mkn_offload(int m,int n,int k,double **A,double **B,double **C){
+    int NUM_TEAMS = 2, THREADS_PER_TEAM = 1;
+    init_C_dev(m,n,C, NUM_TEAMS, THREADS_PER_TEAM);
+    
+    printf("Teams: %d, Th_id: %d. Sum of C initialized: %f\n",omp_get_team_num(), omp_get_thread_num(), get_sum_u(C, m));
+    // #pragma omp target teams distribute parallel for \
+    //     map(to:m,n,k, ) \
+        // shared(m,n,k)
+        for (int row=0; row <m; row++){
+            for (int q=0; q<k; q++){
+                for (int col=0; col < n; col++){
+                    C[row][col] += A[row][q] * B[q][col];
+                }
+            }
+        }
+}
 
 void matmult_lib(int m, int n, int k, double **A, double **B, double **C){
     cblas_dgemm(CblasRowMajor,
--- project_3/matmul/matmult_omp_lib.h
+++ project_3/matmul/matmult_omp_lib.h
@@ -1,3 +1,4 @@
 void init_C(int m,int n,double **C);
-void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C);
 void matmult_lib(int m, int n, int k, double **A, double **B, double **C);
+void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C);
+void matmult_mkn_offload(int m,int n,int k,double **A,double **B,double **C);
