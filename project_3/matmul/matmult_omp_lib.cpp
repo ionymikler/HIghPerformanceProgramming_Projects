db8da959bc38c6dd7e6f37e78a749c8cf5fda620
mihaimnd <mihaivandrei@gmail.com>
2024-01-18 12:21:20 +0100

    remove critical from mkn_omp

--- project_3/matmul/matmult_omp_lib.cpp
+++ project_3/matmul/matmult_omp_lib.cpp
@@ -3,20 +3,15 @@ extern "C"{
 #include <cblas.h>
 #include "matmult_omp_lib.h"
 
-void init_C(int m,int n,double **C){
-    #pragma omp parallel for collapse(2) \
-        shared(C, m,n )
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            #pragma omp critical
-            {
-                C[i][j] = 0.0;
-            }
+void init_C(int m, int n, double **C) {
+    #pragma omp parallel for collapse(2) shared(C, m, n)
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = 0.0;
         }
     }
 }
+
 
 double get_sum_u(double **u, int N){
     double sum = 0;
@@ -31,16 +26,16 @@ double get_sum_u(double **u, int N){
 void matmult_mkn_omp(int m,int n,int k,double **A,double **B,double **C){
     init_C(m,n,C);
     
-    #pragma omp parallel for collapse(2) \
-        shared(A,B,C, m,n,k)
-        for (int row=0; row <m; row++){
-            for (int q=0; q<k; q++){
-                for (int col=0; col < n; col++){
-                    C[row][col] += A[row][q] * B[q][col];
-                }
+    #pragma omp parallel for collapse(2) shared(A, B, C, m, n, k)
+    for (int row=0; row <m; row++) {
+        for (int q=0; q<k; q++) {
+            for (int col=0; col < n; col++) {
+                C[row][col] += A[row][q] * B[q][col];
             }
         }
+    }
 }
+
 
 
 void matmult_lib(int m, int n, int k, double **A, double **B, double **C){
